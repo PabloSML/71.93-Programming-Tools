3d1ba65564061302aa2eaa25c36488c9dce0bb2e
Pablo Smolkin <psmolkin@itba.edu.ar>
2023-05-18 21:26:27 -0300

    Add matlab and python notebooks

---
+++ Matlab/helloworld.m
@@ -0,0 +1,4 @@
+% MATLAB Hello World Script
+
+% Print "Hello, world!" to the console
+fprintf('Hello, world!\n');
---
+++ Matlab/myFunction.m
@@ -0,0 +1,4 @@
+% Custom function definition
+function result = myFunction(a, b)
+    result = a^2 + b^2;
+end
